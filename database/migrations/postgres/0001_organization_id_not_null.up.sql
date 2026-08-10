a9fa570b0d42e1fdf14a2d82108c3f64be2e912d
zowork <sdkwork@zowalk.com>
2026-08-10 19:02:06 +0800

    chore: sync local changes

--- database/migrations/postgres/0001_organization_id_not_null.up.sql
+++ database/migrations/postgres/0001_organization_id_not_null.up.sql
@@ -18,14 +18,17 @@
 
 BEGIN;
 
+ALTER TABLE dr_drive_upload_item ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE dr_drive_upload_item SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE dr_drive_upload_item ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE dr_drive_upload_item ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE dr_drive_file_sensitive_operation ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE dr_drive_file_sensitive_operation SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE dr_drive_file_sensitive_operation ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE dr_drive_file_sensitive_operation ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE dr_drive_sandbox_volume ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE dr_drive_sandbox_volume SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE dr_drive_sandbox_volume ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE dr_drive_sandbox_volume ALTER COLUMN organization_id SET NOT NULL;
